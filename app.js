71830893bb6ce41b57131d34830a8c90a47a6352
Javokhir Shomuratov <javohirshomuradov@gmail.com>
2023-09-23 09:22:13 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express'),
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    app = express();
+
+var myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
+console.log('Using limit: ', myLimit);
+
+app.use(bodyParser.json({limit: myLimit}));
+
+app.all('*', function (req, res, next) {
+
+    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+
+    if (req.method === 'OPTIONS') {
+        // CORS Preflight
+        res.send();
+    } else {
+        var targetURL = "https://api.gatekeeper.software";//req.header('https://api.gatekeeper.software'); // Target-URL ie. https://example.com or http://example.com
+        // if (!targetURL) {
+        //     res.send(500, { error: 'There is no Target-Endpoint header in the request' });
+        //     return;
+        // }
+        console.log(req.header('authorization'));
+        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('authorization'), 'appId': req.header('appId')} },
+            function (error, sresponse, body) {
+                if (error) {
+                    console.error('error: ' + response.statusCode)
+                }
+//                console.log(body);
+            }).pipe(res);
+    }
+});
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function () {
+    console.log('Proxy server listening on port ' + app.get('port'));
+});
